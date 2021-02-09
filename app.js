ef9309a26446663e904cacdd62e2450507ef8f78
Mohit Sharma <mohit7153@gmail.com>
2021-02-09 18:35:59 +0530

    Basic styling added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet')
 
 const db_url = process.env.DB_URL || 'mongodb://localhost:27017/houseOnRent';
-
+//const db_url = 'mongodb://localhost:27017/houseOnRent';
 mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
 const db = mongoose.connection;
