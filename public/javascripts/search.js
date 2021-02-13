f2ec6ba9e7959231955980ed86ccf195710ee26a
Mohit Sharma <mohit7153@gmail.com>
2021-02-13 06:15:47 +0530

    Search house based on rent range

---
+++ public/javascripts/search.js
@@ -0,0 +1,57 @@
+const form = document.getElementById('searchForm');
+const housesContainer = document.getElementById('housesContainer');
+
+
+function populateHouses(min,max){
+  housesContainer.innerHTML = '';
+  for(let house of houses.features){
+    if(min!="" && +house.rent<min){
+      continue;
+    }
+    if(max!=="" && +house.rent>max){
+      continue;
+    }
+    const container = document.createElement('div');
+    container.classList.add('card');
+    container.classList.add('mb-4');
+    container.classList.add('shadow');
+
+    let imgDivText;
+    if(house.images.length>0){ 
+      imgDivText = `<img class="img-fit" alt="" src="${house.images[0].url}">`;
+    }else{ 
+      imgDivText = `<img class="img-fit" src="/images/noHouseImg.png" alt="">`
+    } 
+    container.innerHTML = `
+    <div class="row">
+        <div class="col-sm-5 d-flex flex-column justify-content-center align-items-center">
+            <div class="img-container">`+
+                imgDivText+ 
+            `</div>
+        </div>
+        <div class="col-sm-7 p-3">
+            <div class="card-body">
+                <h5 class="card-title">House Owner's Name: ${house.name}</h5>
+                <small class="text-muted">Rent per month: &#8377;${house.rent}</small>
+                <p class="card-text">Address: ${house.address}</p>
+                <a class="btn btn-primary" href="/houses/${house._id}">Show Details</a>
+            </div>
+        </div>
+    </div>
+    `;
+
+    housesContainer.appendChild(container);
+  }
+};
+
+
+function formfilter(evt){
+  evt.preventDefault();
+
+  populateHouses(minRent.value,maxRent.value);
+};
+
+
+populateHouses();
+
+form.addEventListener('submit',formfilter);
--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -1,19 +1,19 @@
-(function () {
-  'use strict'
-
-  bsCustomFileInput.init()
-
+function executeFormValidation(){
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.need-validation')
   // Loop over them and prevent submission
   Array.from(forms)
-      .forEach(function (form) {
-          form.addEventListener('submit', function (event) {
-              if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-              }
-              form.classList.add('was-validated')
-          }, false)
-      })
-})()
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+};
+
+executeFormValidation();
+
+bsCustomFileInput.init();
